3f8b24620f5ac693b0c4c450a3f681838cecf546
panpat <panospatrinos@gmail.com>
2017-06-02 00:07:05 +0200

    Add files via upload
    minor fixes and additions

--- drn.m
+++ drn.m
@@ -41,11 +41,13 @@ function [x, out] = drn(f, g, s0, gam, opt)
 
     p = [];
     q = [];
-
+    normfpr = zeros(opt.maxit,1);
+    dreval = zeros(opt.maxit,1);
     % initialize cache for QN methods
 
     cache.memory = opt.memory;
     cache.cnt_skip = 0;
+    cache.lambda = opt.lambda;
 
     [x, fx] = f.prox(s, gam);
     cnt_proxf = cnt_proxf + 1;
@@ -53,20 +55,20 @@ function [x, out] = drn(f, g, s0, gam, opt)
     cnt_proxg = cnt_proxg + 1;
     res = z-x;
     DRE_s = DRE(s, x, z, fx, gz, gam);
-
     for it = 1:opt.maxit
 
-        normfpr = vecnorm(x-z);
+        normfpr(it,1) = vecnorm(x-z);
+        dreval(it,1)  = DRE_s;
 
         % display
 
         if opt.display
-            fprintf('%8d%14.4e%14.6e%14.4e\n', it, normfpr, DRE_s, tau);
+            fprintf('%8d%14.4e%14.6e%14.4e\n', it, normfpr(it,1), DRE_s, tau);
         end
 
         % stopping criterion
 
-        if normfpr <= opt.tol
+        if normfpr(it,1) <= opt.tol
             break;
         end
 
@@ -77,6 +79,7 @@ function [x, out] = drn(f, g, s0, gam, opt)
         % perform line-search over the DRE
 
         tau = 1.0;
+        lsfail = true;
 
         for ls_it = 1:opt.maxls
             s1 = s + (1-tau)*opt.lambda*res + tau*dir;
@@ -87,9 +90,20 @@ function [x, out] = drn(f, g, s0, gam, opt)
             res1 = z1-x1;
             DRE_s1 = DRE(s1, x1, z1, fx1, gz1, gam);
             if DRE_s1 <= DRE_s - opt.sigma*vecnorm(z-x)^2
+                lsfail = false;
                 break;
             end
             tau = opt.alpha*tau;
+        end
+        
+        if lsfail
+            s1 = s + opt.lambda*res;
+            [x1, fx1] = f.prox(s1, gam);
+            cnt_proxf = cnt_proxf + 1;
+            [z1, gz1] = g.prox(2*x1 - s1, gam);
+            cnt_proxg = cnt_proxg + 1;
+            res1 = z1-x1;
+            DRE_s1 = DRE(s1, x1, z1, fx1, gz1, gam);
         end
 
         % store pair for QN directions
@@ -103,12 +117,13 @@ function [x, out] = drn(f, g, s0, gam, opt)
         fx = fx1; gz = gz1; % not really needed, but just in case
         res = res1;
         DRE_s = DRE_s1;
-
     end
 
     out.x = x;
+    out.z = z;
     out.iterations = it;
-    out.fpr = normfpr;
+    out.fpr = normfpr(1:it,1);
+    out.dreval = dreval(1:it,1);
     out.cnt_proxf = cnt_proxf;
     out.cnt_proxg = cnt_proxg;
 
