de2d73868dda06e20e27fc90ed5afcc57a72b474
Lorenzo Stella <lorenzostella@gmail.com>
2017-06-02 11:38:29 +0200

    added predicates (for future use)

--- @Proximable/Proximable.m
+++ @Proximable/Proximable.m
@@ -1,3 +1,37 @@
 classdef Proximable < handle
-    
+    methods
+        function p = is_prox_accurate(obj)
+            p = true;
+        end
+        function p = is_separable(obj)
+            p = false;
+        end
+        function p = is_convex(obj)
+            p = false;
+        end
+        function p = is_singleton(obj)
+            p = false;
+        end
+        function p = is_cone(obj)
+            p = false;
+        end
+        function p = is_affine(obj)
+            p = obj.is_singleton();
+        end
+        function p = is_set(obj)
+            p = obj.is_affine() || obj.is_cone();
+        end
+        function p = is_smooth(obj)
+            p = false;
+        end
+        function p = is_quadratic(obj)
+            p = false;
+        end
+        function p = is_generalized_quadratic(obj)
+            p = obj.is_quadratic() || obj.is_affine();
+        end
+        function p = is_strongly_convex(obj)
+            p = false;
+        end
+    end
 end
