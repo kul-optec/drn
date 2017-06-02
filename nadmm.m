5a8b20ba8b88104af84f747e1d50a9818225921a
Lorenzo Stella <lorenzostella@gmail.com>
2017-06-02 17:00:54 +0200

    oops

--- nadmm.m
+++ nadmm.m
@@ -6,16 +6,6 @@
 
 function [s, out] = nadmm(f, g, A, B, b, s0, gam, opt)
 
-    muA = get_gram_diagonal(A');
-    if muA == 0
-        error('A must be such that A''*A = mu*I, for some mu > 0');
-    end
-
-    muB = get_gram_diagonal(B');
-    if muB == 0
-        error('B must be such that B''*B = mu*I, for some mu > 0');
-    end
-
     phi1 = EpiCompose(f, A);
     phi2 = ScaleTranslate(EpiCompose(g, B), -1, b);
 
