4515d3b8d12dfdf078c97a2a347cf27cfaaff279
Lorenzo Stella <lorenzostella@gmail.com>
2017-06-03 15:27:12 +0200

    requiring rho in place of gamma

--- nadmm.m
+++ nadmm.m
@@ -4,14 +4,14 @@
 %       subject to Ax + Bz = b
 %
 
-function [s, out] = nadmm(f, g, A, B, b, s0, gam, opt)
+function [s, out] = nadmm(f, g, A, B, b, s0, rho, opt)
 
     if nargin < 8, opt = struct(); end
 
     phi1 = EpiCompose(f, A);
     phi2 = ScaleTranslate(EpiCompose(g, B), -1, b);
 
-    [s, out] = drn(phi1, phi2, s0, gam, opt);
+    [s, out] = drn(phi1, phi2, s0, 1/rho, opt);
 
-    % TODO: get primal solution
+    % TODO: get primal solution (x, z)
 end
