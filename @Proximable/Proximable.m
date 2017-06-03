86e0362fb77eae857410b6f264a1a28bb239e6fa
Lorenzo Stella <lorenzostella@gmail.com>
2017-06-03 18:35:38 +0200

    fixed bug

--- @Proximable/Proximable.m
+++ @Proximable/Proximable.m
@@ -36,7 +36,7 @@ classdef Proximable < handle
     end
     methods(Static)
         function mu = get_gram_diagonal(M)
-            mus = M*(M'*ones(size(M, 2), 1));
+            mus = M*(M'*ones(size(M, 1), 1));
             if (max(mus)-min(mus))/(1+abs(min(mus))) > 1e-14
                 mu = 0;
             else
