bdc01e1401f5d28355b417c96b7b649ceacc97cb
Lorenzo Stella <lorenzostella@gmail.com>
2017-06-06 14:57:35 +0200

    fixed bug, updated tests

--- @EpiCompose/prox.m
+++ @EpiCompose/prox.m
@@ -7,7 +7,7 @@ function [p, v] = prox(obj, s, gam)
                 obj.L_prox = chol(obj.Q + (1/gam)*obj.A'*obj.A); % do differently for sparse?
             end
             obj.x = obj.L_prox\(obj.L_prox'\((obj.A'*s)/gam - obj.q));
-            v = 0.5*(x'*(obj.Q*x)) + obj.q'*x; % can we save something here?
+            v = 0.5*(obj.x'*(obj.Q*obj.x)) + obj.q'*obj.x; % can we save something here?
         case 2 % f = QuadraticOverAffine
             error('not implemented');
         case 3 % f = Any proximable function and A'*A = mu*Id, mu > 0
--- private/test_boxqp.m
+++ private/test_boxqp.m
@@ -19,13 +19,15 @@ opt.maxit = 1000;
 % Solve using DRN
 
 gam = 0.95/Lf;
-[x_drn, out_drn] = drn(f, g, zeros(n, 1), gam, opt);
+out_drn = drn(f, g, zeros(n, 1), gam, opt);
+x_drn = out_drn.z;
 
 assert(norm(x_drn - x_star)/(1+norm(x_star)) <= 1e-8);
 
 % Solve using NADMM
 
 rho = 1/gam;
-[x_nadmm, out_nadmm] = nadmm(f, g, 1, -1, 0, zeros(n, 1), rho, opt);
+out_nadmm = nadmm(f, g, 1, -1, 0, zeros(n, 1), rho, opt);
+x_nadmm = out_nadmm.drn.z;
 
 assert(norm(x_nadmm - x_star)/(1+norm(x_star)) <= 1e-8);
