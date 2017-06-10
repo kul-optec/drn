faa53d65fec7ff7e46504876af3b996defe64af3
Lorenzo Stella <lorenzostella@gmail.com>
2017-06-10 18:11:54 +0200

    added large sparse case using pcg

--- @Quadratic/Quadratic.m
+++ @Quadratic/Quadratic.m
@@ -5,9 +5,21 @@ classdef Quadratic < Proximable
         Q, q % Hessian and linear term
         L_prox % stores Cholesky factor for prox
         gam_prox % stores most recently used parameter for prox
+        flag_sparse
+        flag_large
     end
     methods
         function obj = Quadratic(Q, q)
+            if issparse(Q)
+                obj.flag_sparse = true;
+            else
+                obj.flag_sparse = false;
+            end
+            if size(Q, 2) > 5000
+                obj.flag_large = true;
+            else
+                obj.flag_large = false;
+            end
             obj.Q = Q;
             obj.q = q;
             obj.gam_prox = 0;
--- @Quadratic/prox.m
+++ @Quadratic/prox.m
@@ -1,11 +1,23 @@
 function [p, v] = prox(obj, x, gam)
-    if gam ~= obj.gam_prox % more robust test?
-        % factor matrix when gam changes (or at first call)
-        obj.gam_prox = gam;
-        n = length(x);
-        I = speye(n);
-        obj.L_prox = chol(I + gam*obj.Q); % do differently for sparse?
+    n = length(x);
+    I = speye(n);
+    if obj.flag_large
+        [p, flag] = pcg(I + gam*obj.Q, x - gam*obj.q);
+    else
+        if gam ~= obj.gam_prox % more robust test?
+            % factor matrix when gam changes (or at first call)
+            obj.gam_prox = gam;
+            if obj.flag_sparse
+                obj.L_prox = ldlchol(I + gam*obj.Q);
+            else
+                obj.L_prox = chol(I + gam*obj.Q); % do differently for sparse?
+            end
+        end
+        if obj.flag_sparse
+            p = ldlsolve(obj.L_prox, x - gam*obj.q);
+        else
+            p = obj.L_prox\(obj.L_prox'\(x - gam*obj.q));
+        end
     end
-    p = obj.L_prox\(obj.L_prox'\(x - gam*obj.q));
     v = 0.5*(p'*(obj.Q*p)) + obj.q'*p; % can we save something here?
 end
